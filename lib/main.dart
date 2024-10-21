88a1e490845c12a2f0036317e2eb8478555eb5ad
joseb <josebotero103@gmail.com>
2024-10-21 00:25:14 -0500

    Buen funcionamiento

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'dart:convert';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
